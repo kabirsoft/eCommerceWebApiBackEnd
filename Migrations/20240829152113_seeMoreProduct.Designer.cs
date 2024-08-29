a69fc1afce402102bf0fd9880efb96f5b776d3ec
mka198-cloud <mka198@uit.no>
2024-08-29 17:23:47 +0200

    Added category table. Seed data.

--- Migrations/20240829152113_seeMoreProduct.Designer.cs
+++ Migrations/20240829152113_seeMoreProduct.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceWebApiBackEnd.Data;
 
@@ -10,9 +11,11 @@ using eCommerceWebApiBackEnd.Data;
 namespace eCommerceWebApiBackEnd.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240829152113_seeMoreProduct")]
+    partial class seeMoreProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
